cab1c87f70b49850d96364865cda213304b27637
jrs-neto <jrd.santosneto@gmail.com>
2026-02-02 14:49:29 -0300

    Exercício de Collection Queue e Collection Stack

---
+++ aula_02/exercicios/01-fila.ts
@@ -0,0 +1,51 @@
+import Queue = require("../queue");
+import leia = require("readline-sync");
+
+const fila = new Queue<string>();
+let opcoes: string;
+
+do {
+  console.log("************************************************************");
+  console.log("1 - Adicionar Cliente na Fila");
+  console.log("2 - Listar todos os Clientes");
+  console.log("3 - Retirar Cliente da Fila");
+  console.log("0 - Sair");
+  console.log("************************************************************");
+
+  opcoes = leia.question("Digite a opcao desejada.");
+
+  switch (opcoes) {
+    case "1":
+      fila.enqueue(leia.question("Digite o nome do Cliente: "));
+      fila.printQueue();
+      console.log("Cliente Adicionado!");
+      break;
+
+    case "2":
+      if (fila.isEmpty()) {
+        console.log("A fila está vazia.");
+      } else {
+        fila.printQueue();
+      }
+      break;
+
+    case "3":
+      if (fila.isEmpty()) {
+        console.log("A fila está vazia.");
+      } else {
+        const cliente = fila.dequeue();
+        console.log(`Cliente ${cliente} foi chamado.`);
+        console.log("Cliente retirado da fila");
+        fila.printQueue();
+      }
+      break;
+
+    case "0":
+      console.log("Programa finalizado.");
+      break;
+
+    default:
+      console.log("Opção Inválida.");
+      break;
+  }
+} while (opcoes !== "0");
---
+++ aula_02/exercicios/02-pilha.ts
@@ -0,0 +1,50 @@
+import stack = require("../stack");
+import leia = require("readline-sync");
+
+const pilha = new stack<string>();
+let opcoes: string;
+
+do {
+  console.log("************************************************************");
+  console.log("1 - Adicionar Livro na pilha");
+  console.log("2 - Listar todos os Livros");
+  console.log("3 - Retirar Livro da pilha");
+  console.log("0 - Sair");
+  console.log("************************************************************");
+
+  opcoes = leia.question("Digite a opcao desejada.");
+
+  switch (opcoes) {
+    case "1":
+      pilha.push(leia.question("Digite o nome do Livro: "));
+      pilha.printStack();
+      console.log("Livro Adicionado!");
+      break;
+
+    case "2":
+      if (pilha.isEmpty()) {
+        console.log("A pilha está vazia.");
+      } else {
+        pilha.printStack();
+      }
+      break;
+
+    case "3":
+      if (pilha.isEmpty()) {
+        console.log("A pilha está vazia.");
+      } else {
+        const livro = pilha.pop();
+        console.log(`O Livro ${livro} foi retirado da pilha`);
+        pilha.printStack();
+      }
+      break;
+
+    case "0":
+      console.log("Programa finalizado.");
+      break;
+
+    default:
+      console.log("Opção Inválida.");
+      break;
+  }
+} while (opcoes !== "0");
